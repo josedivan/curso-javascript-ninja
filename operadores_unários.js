9cd36374dd4e312942135cea30e09ece6ff21e0e
Edivan <edivanlima23@gmail.com>
2021-04-09 11:01:37 -0300

    aulas-javascript-ninja

--- operadores_unários.js
+++
@@ -1,6 +0,0 @@
-+"3"; // vai retornar 3 , pois o sina de mais antes converte para number
-
-"ed" + "ivan"; // vai concatenar os dois valores e vai retornar Edivan
-3 + +"3"; // vai retornar 6, porque o + antes converte e string "3"
-var x = 3;
--x; // vai retornar menos -3
