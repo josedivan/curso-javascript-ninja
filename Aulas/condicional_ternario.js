55e1c2f0b3918523eb30396d222c0e1928a04907
Edivan <edivanlima23@gmail.com>
2021-04-11 20:07:27 -0300

    condicional ternário

---
+++ Aulas/condicional_ternario.js
@@ -0,0 +1,17 @@
+// condicional ternário substitui o if em estrutura de repetoções pequenas 
+var teste1 = 1===2 ? true: false 
+var teste2 = 2===2 ? true: false 
+
+pessoa = {sexo: "Masculino", idade: 32}
+var carro = false ? "porta": "janela"; // a primeira instrução é false e então retotna janela true;
+
+var sexo = pessoa.sexo === "Masculino" ? true: false 
+var sexo = pessoa.sexo === "masculino"? "a": "o"
+
+
+
+
+console.log(teste1)
+console.log(teste2)
+console.log(pessoa)
+console.log(carro)
