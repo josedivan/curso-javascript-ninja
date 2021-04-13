bd26db9e7fbfc86486e4e47733937e49df71b401
Edivan <edivanlima23@gmail.com>
2021-04-13 18:05:46 -0300

    retorno de funções e objetos

---
+++ Aulas/retorno_funcoes_comArrays_e_objetos.js
@@ -0,0 +1,19 @@
+function myfunction (){
+  return 1
+}
+
+ function myfunction(){
+   return "string"
+ }
+ myfunction()
+
+function myfunction (){
+  return 1;
+  return 2;
+}
+
+function myfunction(){
+  return [1,2,3];
+}
+
+console.log(myfunction()[0])
