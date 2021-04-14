f7c7d3996dfb662ac2e98e190b23efd51020bed1
Edivan <edivanlima23@gmail.com>
2021-04-14 18:15:41 -0300

    parametros de funções e objetos

---
+++ Aulas/parametros_de_funcoes_e_objetos.js
@@ -0,0 +1,14 @@
+var obj = {
+propriedade: 'Edivan',
+curso: 'javascript ninja',
+ninja : true
+}
+
+function myfunction1(arg){
+  return arg.curso
+}
+
+
+console.log(obj.propriedade)
+console.log(obj.curso)
+console.log(myfunction1(obj))
