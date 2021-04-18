f11dc70cb71ebb2f620f66385d06fce57c5f34ae
Edivan <edivanlima23@gmail.com>
2021-04-18 20:20:53 -0300

    Operador de módulos

---
+++ Aulas/operador_modulo.js
@@ -0,0 +1,14 @@
+3 / 3 // 1
+
+4 / 2 // 2
+5 / 2 // 2.5
+5 % 2 // resto 1 
+
+numeros = 0
+while (numeros < 30) {
+  numeros % 2 === 0 ? console.log(numeros) : '';
+  numeros++
+}
+
+console.log(numeros)
+
