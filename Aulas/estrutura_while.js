611f17afc79e5ce48ac0385cdd6df8023ed8d9bc
Edivan <edivanlima23@gmail.com>
2021-04-18 13:08:22 -0300

    Estrurura condicional while

---
+++ Aulas/estrutura_while.js
@@ -0,0 +1,20 @@
+var contador = 1
+while (contador < 5) {
+  console.log(contador)
+  contador++
+}
+// Decremento --
+var contador2 = 10
+while (contador2 > 0) {
+  console.log(contador2--)
+}
+// OUTRA FORMA DE DECREMENTO 2
+
+var contador2 = 10
+while (contador2--) {
+  console.log(contador2)
+}
+
+//console.log(contador)
+
+console.log(contador2)
