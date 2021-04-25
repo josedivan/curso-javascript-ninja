9ceecb20809334e5aa5cf13c06f4666e7e454bda
Edivan <edivanlima23@gmail.com>
2021-04-25 17:36:59 -0300

    nomeação de funções anonima

---
+++ Aulas/nomeacao_de_funcoes.js
@@ -0,0 +1,18 @@
+var func = function () {
+  return 'Hi'
+}
+console.log(func())
+
+func01 = function () {
+
+}
+func01()
+console.log(func01.nome)
+
+var fun = function func() { }
+func()
+func.nome
+function myFunction2() {
+  return 'function 2'
+}
+myFunction2.nome
