00b4caec9d9cbba07a694e9146d95c7a56c06932
Edivan <edivanlima23@gmail.com>
2021-06-04 19:51:42 -0300

    loops e laços

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -170,3 +170,4 @@ resolver o problema corretamente.
 
 })();
 
+
---
+++ challenge-10/lacos_e_loops.js
@@ -0,0 +1,10 @@
+var car = {
+  cor: ' vermelho',
+  ano: '2021',
+  marcar: 'Jeep'
+
+}
+
+for (var pro in car) {
+  console.log(pro)
+}
